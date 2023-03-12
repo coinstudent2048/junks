adfb32840c38d70dc558510f42afd67ff03c826b
coinstudent2048 <87281755+coinstudent2048@users.noreply.github.com>
2023-03-12 01:57:12 +0000

    Update non_prime_fields.py
    First pass

--- non_prime_fields.py
+++ non_prime_fields.py
@@ -1,7 +1,7 @@
 # Construction of Non-prime fields
 # Python 3
 #
-# Unoptimized and no error-checking. Coded for clarity instead.
+# Minimal optimization and error-checking. Coded for clarity instead.
 
 # Field of prime order l. No prime checking of l
 class PrimeField:
@@ -12,13 +12,10 @@ class PrimeField:
         else:
             raise TypeError
 
-    # Multiplicative inversion, with an option to let 1/0 = 0 if you're into that
-    def invert(self, allow_zero=False):
+    # Multiplicative inversion
+    def invert(self):
         if self.x == 0:
-            if allow_zero:
-                return PrimeField(0, self.l)
-            else:
-                raise ZeroDivisionError
+            raise ZeroDivisionError
         return PrimeField(pow(self.x, self.l - 2, self.l), self.l)
 
     # Addition
@@ -96,9 +93,254 @@ class PrimeField:
     def __neg__(self):
         return PrimeField(-self.x, self.l)
 
+# Polynomial here is a list of PrimeField of order l [c0, c1, ..., cN]
+# which means c0 + c1 * x + ... + cN * x ** N (mod l)
 class Polynomial:
-    pass
+    def __init__(self, coeffs, l):
+        if isinstance(coeffs, list) and isinstance(l, int) and l > 0:
+            first_trail_zero = 1
+            for i, j in enumerate(coeffs):
+                if not (isinstance(j, PrimeField) and j.l == l):
+                    raise TypeError
+                if i > 0 and j.x != 0:
+                    first_trail_zero = i + 1
+            self.coeffs = coeffs[0: first_trail_zero] # remove trailing zeros
+            self.l = l
+        else:
+            raise TypeError
 
-# Field of non-prime order l. No prime checking of l
+    # Addition
+    def __add__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            if len(self.coeffs) > len(y.coeffs):
+                extra_coeffs = self.coeffs[len(y.coeffs): len(self.coeffs)]
+            else:
+                extra_coeffs = y.coeffs[len(self.coeffs): len(y.coeffs)]
+            return Polynomial([i + j for i, j in zip(self.coeffs, y.coeffs)] + extra_coeffs, self.l)
+        return NotImplemented
+
+    # Subtraction
+    def __sub__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            if len(self.coeffs) > len(y.coeffs):
+                extra_coeffs = self.coeffs[len(y.coeffs): len(self.coeffs)]
+            else:
+                extra_coeffs = [-i for i in y.coeffs[len(self.coeffs): len(y.coeffs)]]
+            return Polynomial([i - j for i, j in zip(self.coeffs, y.coeffs)] + extra_coeffs, self.l)
+        return NotImplemented
+
+    # Multiplication
+    # Note: This is naive. FFT is a faster algorithm for this.
+    def __mul__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            zero_coeffs = [PrimeField(0, self.l)]
+            product = Polynomial(zero_coeffs, self.l)
+            for i, j in enumerate(y.coeffs):
+                product += Polynomial(zero_coeffs * i + [k * j for k in self.coeffs], self.l)
+            return product
+        return NotImplemented
+
+    # Divmod (Euclidean division)
+    # Note: This is naive.
+    def divmod(self, y, mod_only=False):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            zero_coeffs = [PrimeField(0, self.l)]
+            if y.coeffs == zero_poly:
+                raise ZeroDivisionError
+            modulo = self
+            len_diff = len(modulo.coeffs) - len(y.coeffs)
+            if not mod_only:
+                quotient = zero_coeffs * (len_diff + 1)
+
+            while len_diff >= 0:
+                quot_coeff = modulo.coeffs[-1] * y.coeffs[-1].invert()
+                if not mod_only:
+                    quotient[len_diff] = quot_coeff
+                diff_list = [i - quot_coeff * j for i, j in zip(modulo.coeffs[len_diff: len(modulo.coeffs)], y.coeffs)]
+                modulo = Polynomial(modulo.coeffs[0: len_diff] + diff_list, self.l)
+                len_diff = len(modulo.coeffs) - len(y.coeffs)
+                if modulo.coeffs == zero_coeffs:
+                    len_diff -= 1 # here, zero polynomial has 'length' 0
+
+            if mod_only:
+                return None, modulo
+            else:
+                return Polynomial(quotient, self.l), modulo
+        return NotImplemented
+
+    # Integer exponentiation
+    def __pow__(self, y):
+        if isinstance(y, int) and y >= 0:
+            if y == 0:
+                return Polynomial([PrimeField(1, self.l)], self.l)
+            Q = self.__pow__(y // 2)
+            Q = Q.__mul__(Q)
+            if y & 1:
+                Q = self.__mul__(Q)
+            return Q
+        return NotImplemented
+
+    # Equality
+    def __eq__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            return self.coeffs == y.coeffs
+        raise TypeError
+
+    # Inequality
+    def __ne__(self,y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            return self.coeffs != y.coeffs
+        raise TypeError
+
+    # Less-than comparison. This will be the basis for other comparison dunders
+    # This is based on degree of polynomial and ordering of underlying PrimeField
+    def __lt__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            if len(self.coeffs) != len(y.coeffs):
+                return len(self.coeffs) < len(y.coeffs)
+            for i, j in zip(self.coeffs, y.coeffs):
+                if i != j:
+                    return i < j
+            return False # this means that self == y
+        raise TypeError
+
+    # Greater-than comparison
+    def __gt__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            return not (self.__lt__(y) or self.__eq__(y))
+        raise TypeError
+
+    # Less-than-or-equal comparison
+    def __le__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            return self.__lt__(y) or self.__eq__(y)
+        raise TypeError
+
+    # Greater-than-or-equal comparison
+    def __ge__(self, y):
+        if isinstance(y, Polynomial) and self.l == y.l:
+            return not self.__lt__(y)
+        raise TypeError
+
+    # String representation
+    def __repr__(self):
+        return f'{ [i.x for i in self.coeffs] } (mod { self.l })'
+
+    # Negation
+    def __neg__(self):
+        return self.__mul__(Polynomial([PrimeField(-1, self.l)], self.l))
+
+def zero_poly(l):
+    return Polynomial([PrimeField(0, l)], l)
+
+def one_poly(l):
+    return Polynomial([PrimeField(1, l)], l)
+
+# Field of non-prime order defined using irreducible polynomial p. No checking of irreducibility
 class NonPrimeField:
-    pass
+    def __init__(self, x, p):
+        if isinstance(x, Polynomial) and isinstance(p, Polynomial) and x.l == p.l:
+            _, self.x = x.divmod(p, True)
+            self.p = p
+        else:
+            raise TypeError
+
+    # Multiplicative inversion
+    def invert(self):
+        zero_poly_npf = zero_poly(self.p.l)
+        if self.x == zero_poly_npf:
+            raise ZeroDivisionError
+        # Source: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Simple_algebraic_field_extensions
+        # Retrieved: 10/03/2023
+        t = zero_poly_npf
+        newt = one_poly(self.p.l)
+        r = self.p
+        newr = self.x
+
+        while newr != zero_poly_npf:
+            quot, rem = r.divmod(newr)
+            r, newr = newr, rem
+            t, newt = newt, t - quot * newt
+
+        if len(r.coeffs) > 1:
+            raise ValueError("Either self.p is not irreducible or self.x is a multiple of self.p.")
+
+        if self.p.l > 2:
+            return Polynomial([r.coeffs[0].invert()], self.p.l) * t
+        else: # optimization
+            return t
+
+    # Addition
+    def __add__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return NonPrimeField(self.x + y.x, self.p)
+        return NotImplemented
+
+    # Subtraction
+    def __sub__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return NonPrimeField(self.x - y.x, self.p)
+        return NotImplemented
+
+    # Multiplication (possibly by a polynomial mod l)
+    def __mul__(self, y):
+        if isinstance(y, Polynomial) and self.p.l == y.l:
+            return NonPrimeField(self.x * y, self.l)
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return NonPrimeField(self.x * y.x, self.l)
+        return NotImplemented
+
+    def __rmul__(self, y):
+        if isinstance(y, Polynomial) and self.p.l == y.l:
+            return self * y
+        return NotImplemented
+
+    # Integer exponentiation
+    def __pow__(self, y):
+        if isinstance(y, int) and y >= 0:
+            return NonPrimeField(self.x ** y, self.p)
+        return NotImplemented
+
+    # Equality
+    def __eq__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return self.x == y.x
+        raise TypeError
+
+    # Inequality
+    def __ne__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return self.x != y.x
+        raise TypeError
+
+    # Less-than comparison
+    def __lt__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return self.x < y.x
+        raise TypeError
+
+    # Greater-than comparison
+    def __gt__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return self.x > y.x
+        raise TypeError
+
+    # Less-than-or-equal comparison
+    def __le__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return self.x <= y.x
+        raise TypeError
+
+    # Greater-than-or-equal comparison
+    def __ge__(self, y):
+        if isinstance(y, NonPrimeField) and self.p == y.p:
+            return self.x >= y.x
+        raise TypeError
+
+    # String representation
+    def __repr__(self):
+        return f'{ self.x } (mod { self.p })'
+
+    # Negation
+    def __neg__(self):
+        return NonPrimeField(-self.x, self.p)
